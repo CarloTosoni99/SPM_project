0eb249771dc35218e985f1017a1dcb45264463d2
Carlo <tosonicarlo99@gmail.com>
2022-07-27 21:50:42 +0200

    another commit

--- timer.cpp
+++ timer.cpp
@@ -9,8 +9,9 @@ using namespace std;
 int main () {
     string s;
     cout << "Please enter your name: ";
-    getline(cin, s);
+    getline(cin, s);ù
     cout << "Hello " << s << endl;
     cout << "Have fun in cs2010...!..." << endl;
     return 0;
 }
+//changing lol ._.
