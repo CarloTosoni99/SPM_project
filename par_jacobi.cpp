ccba5f58714840e3c080d3eecdd35147d94d444a
Carlo <tosonicarlo99@gmail.com>
2022-07-29 18:50:38 +0200

    added first implementation of the parallel version

--- par_jacobi.cpp
+++ par_jacobi.cpp
@@ -1,12 +1,11 @@
-#include <vector>
 #include <iostream>
-#include <functional>
 #include <cmath>
-
+#include <thread>
+#include <functional>
+#include <barrier>
+#include <vector>
 
 #include "my_timer.cpp"
-
-//using namespace std::chrono_literals;
 
 
 float compute_magnitude(std::vector<float>& x, std::vector<float>& xo, int n) {
@@ -27,41 +26,63 @@ float compute_magnitude(std::vector<float>& x, std::vector<float>& xo, int n) {
     return num / den;
 }
 
-void seq_jacobi(std::vector<float>& a, std::vector<float>& b, std::vector<float>& x, int n, int n_iter, float tol, int ch_conv) {
 
-    std::cout << "entering the jacobi method... " << std::endl;
+void jacobi(std::vector<float>& a, std::vector<float>& b, std::vector<float>& x, int n, int n_iter, float tol, int ch_conv, int nw) {
+
+    std::cout << "entering the Jacobi method... " << std::endl;
 
     int k = 1;
     std::vector<float> xo = x;
-    while (k <= n_iter) {
-        for(int i = 0; i < n; i++) {
-            float val = 0.0;
-            for(int j = 0; j < n; j++) {
-                if (i != j)
-                    val = val + a[i*n + j]*xo[j];
+
+
+    std::barrier bar(nw, [&]() {k = k +1; xo = x;});
+
+    std::function<void(int)> parjac = [&](int thr_n){
+
+        while (k <= n_iter) {
+            //std::cout << "this is thread " << thr_n << " i'm starting iter number " << k << std::endl;
+            for (int i = thr_n; i < n; i += nw) {
+                float val = 0.0;
+                for (int j = 0; j < n; j++) {
+                    if (i != j)
+                        val = val + a[i*n + j]*xo[j];
+                }
+                x[i] = (1/a[(n+1)*i])*(b[i]-val);
             }
-            x[i] = (1/a[(n+1)*i])*(b[i]-val);
-        }
-        if (ch_conv != 0)
-            if (compute_magnitude(std::ref(x), std::ref(xo), n) < tol) {
+            if (ch_conv != 0)
+                std::cout << "don't read this line" << std::endl;
+            /*if (compute_magnitude(std::ref(x), std::ref(xo), n) < tol) {
                 std::cout << "condition for convergence is satisfied" << std::endl;
-                return;
-            }
-        k++;
-        xo = x;
+                return x;
+            }*/
+            bar.arrive_and_wait();
+        }
+
+        return;
+    };
+
+    std::vector<std::thread> tvec(nw);
+    for (int i = 0; i < nw; i++) {
+        tvec[i] = std::thread(parjac, i);
     }
 
-    std::cout << "jacobi is done" << std::endl;
+    for(std::thread &thr : tvec) {
+        thr.join();
+    }
+
+    std::cout << "Jacobi is done" << std::endl;
     return;
 }
 
-int main(int argc, char *argv[]) {
+
+int main(int argc, char *argv[]){
 
     int seed = std::stoul(argv[1]); //seed to generate random numbers
     int n = std::stoul(argv[2]); //linear system's dimension
     int n_iter = std::stoul(argv[3]); //maximum number of iterations
     int ch_conv = std::stoul(argv[4]); //if it's 1 the programm will check the convergence of jacobi at each iteration, if it's 0 it will not
-    float tol = std::atof(argv[5]); //maximum tolerance for convergence, the program will use this value only if ch_conv == 1
+    float tol = std::atof(argv[5]); //maximum tolerance for convergence, the program will use this value only if ch_conv ==
+    int nw = std::stoul(argv[6]); //parallel degree
 
     srand(seed);
     std::vector<float> a(n*n);
@@ -92,24 +113,25 @@ int main(int argc, char *argv[]) {
         b[i] = lo + static_cast<float> (rand() / static_cast<float>(RAND_MAX/(hi-lo)));;
     }
 
-
-
     my_timer timer;
     timer.start_timer();
-    seq_jacobi(std::ref(a), std::ref(b), std::ref(x), n, n_iter, tol, ch_conv);
+    jacobi(std::ref(a), std::ref(b), std::ref(x), n, n_iter, tol, ch_conv, nw);
     time_t elapsed = timer.get_time();
     std::cout << "Elapsed time: " << elapsed << std::endl;
 
+
     // optional to check the error
-    /*for(int i = 0; i < n; i++){
+    /*std::cout << "error check enabled" << std::endl;
+    for(int i = 0; i < n; i++){
         float v = 0.0;
         for(int j = 0; j < n; j++){
             v = a[i*n + j]*x[j] + v;
         }
         v = v - b[i];
-        std::cout << "Error at row i " << v << std::endl;
+        if ( v > 0.001 )
+            std::cout << "Error at row i " << v << std::endl;
     }*/
-    
-    
+
+
     return 0;
 }
